812c786386e07266788e48c349d628595c886470
zalalahovnik <zala.lahovnik@gmail.com>
2024-05-11 14:54:06 +0200

    feat: add cors

--- src/ExpressJS/app.js
+++ src/ExpressJS/app.js
@@ -3,10 +3,13 @@ import mongoose from 'mongoose';
 import connectDB from './db.js';
 import Recipe from './models/recipe.js';
 import recipeRoute from './routes/recipesRoute.js'
+import cors from 'cors'
 
 const app = express();
 const PORT = 5000;
 connectDB();
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Welcome to Voice Chef!');
