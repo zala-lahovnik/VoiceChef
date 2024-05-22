2417ea295b4eca33909c6da8faab680d850ffc4b
zalalahovnik <zala.lahovnik@gmail.com>
2024-05-22 23:52:09 +0200

    feat: add styles for main page

--- src/odjemalec/src/components/RecipeDisplay/RecipeDisplay.tsx
+++ src/odjemalec/src/components/RecipeDisplay/RecipeDisplay.tsx
@@ -13,25 +13,33 @@ const RecipeDisplay:FC<RecipeDisplayProps> = ({recipe}) => {
     <Grid container sx={{
       display: 'flex',
       flexDirection: 'column',
-      backgroundColor: '#fff',
-      borderRadius: 2,
-      // padding: 1,
+      backgroundColor: '#1F1D2B',
+      borderRadius: '16px',
       height: '100%',
       width: '100%',
-      boxShadow: '5px 10px 15px #d3d3d3',
-      border: '1px solid #d3d3d3'
+      alignItems: 'center'
     }}>
-      <img src={recipe.img} style={{
-        maxWidth: '100%',
-        maxHeight: '100%',
-        borderRadius: '8px'
-      }} />
-      <Box sx={{fontWeight: 500, fontSize: '18px', padding: 1, position: 'relative'}}>
+      <Box sx={{
+        padding: '12px',
+        background: 'radial-gradient(circle at 100%, #3f2e20, #563f2d 50%, #2a1e15)',
+        marginTop: '-60px',
+        borderRadius: '100%',
+        border: '2px solid #3f2e20',
+        backgroundColor: '#2a1e15'
+      }}>
+        <img src={recipe.img} style={{
+          width: '150px',
+          height: '150px',
+          borderRadius: '50%',
+          border: '1px solid #3f2e20'
+        }} />
+      </Box>
+      <Box sx={{fontWeight: 700, fontSize: '18px', padding: 1, paddingTop: 2, position: 'relative', letterSpacing: '1px'}}>
         <div style={{backgroundColor: '#d17a22', borderRadius: '8px', width: 'fit-content', padding: '4px 8px', color: 'white'}}>
           {recipe.category}
         </div>
       </Box>
-      <Typography sx={{fontWeight: 700, fontSize: '24px', padding: 1, marginBottom: 1}}>
+      <Typography sx={{fontSize: '24px', padding: 1, marginBottom: 1, textAlign: 'center', color: '#fff'}}>
         {recipe.title}
       </Typography>
     </Grid>
--- src/odjemalec/src/pages/RecipesPage.tsx
+++ src/odjemalec/src/pages/RecipesPage.tsx
@@ -1,21 +1,98 @@
 import React, { FC, useEffect, useState } from "react";
 import { Recipe } from "../utils/recipeTypes";
 import voiceChefApi from "../utils/axios";
-import { Grid, Typography } from "@mui/material";
+import {
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  outlinedInputClasses,
+  Select,
+  SelectChangeEvent, styled, TextField,
+  Typography
+} from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import RecipeDisplay from "../components/RecipeDisplay/RecipeDisplay";
 import SideMenu from "../components/SideMenu/SideMenu";
+import './recipes-page.css'
 
 type RecipesPageProps = {}
 
+const StyledTextField = styled(TextField)({
+  '& .MuiOutlinedInput-root': {
+    color: 'white',
+    backgroundColor: '#2D303E',
+    borderRadius: '16px',
+    '& fieldset': {
+      borderColor: '#2D303E',
+    },
+    '&:hover fieldset': {
+      borderColor: '#2D303E',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: '#2D303E',
+    },
+  },
+  '& .MuiInputLabel-root': {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  '& .MuiInputLabel-root.Mui-focused': {
+    color: 'white',
+  },
+});
+
+const WhiteSelect = styled(Select)(({ theme }) => ({
+  color: 'white',
+  borderRadius: '16px',
+  backgroundColor: '#2D303E',
+  fontWeight: 'bold',
+  letterSpacing: '2px',
+  '& .MuiOutlinedInput-notchedOutline': {
+    borderColor: '#2D303E',
+  },
+  '&:hover .MuiOutlinedInput-notchedOutline': {
+    borderColor: '#2D303E',
+  },
+  '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+    borderColor: '#2D303E',
+  },
+  '& .MuiInputLabel-root': {
+    color: '#2D303E',
+    fontWeight: 'bold',
+  },
+  '&.Mui-focused .MuiInputLabel-root': {
+    color: '#2D303E',
+  },
+  '& .MuiSvgIcon-root': {
+    color: '#2D303E',
+  },
+}));
+
+
 const RecipesPage: FC<RecipesPageProps> = () => {
   const [recipes, setRecipes] = useState<Array<Recipe>>([]);
+  const [categories, setCategories] = useState<Array<string>>([])
+  const [selectedCategory, setSelectedCategory] = useState<string>('--')
   const navigate = useNavigate();
 
   const fetchRecipes = async () => {
     try {
       const result = await voiceChefApi.get('/recipes');
       setRecipes(result.data);
+
+      const tempCategoriesArray = result.data.map((recipe: Recipe) => {
+        return recipe.category
+      })
+
+      const uniqueArray = tempCategoriesArray.reduce((accumulator: Array<string>, currentValue: string) => {
+        if (!accumulator.includes(currentValue)) {
+          accumulator.push(currentValue);
+        }
+        return accumulator;
+      }, []);
+
+      setCategories(uniqueArray)
     } catch (error) {
       console.error('Error fetching recipes', error);
     }
@@ -28,6 +105,10 @@ const RecipesPage: FC<RecipesPageProps> = () => {
   const handleOnClickOnRecipe = (id: string) => {
     navigate(`/recipe/${id}`);
   };
+
+  const handleCategoryChange = (event: SelectChangeEvent<unknown>) => {
+    setSelectedCategory(event.target.value as string);
+  }
 
   return (
     <Grid container sx={{
@@ -42,9 +123,47 @@ const RecipesPage: FC<RecipesPageProps> = () => {
         <SideMenu />
       </Grid>
 
-      <Grid item xs={11} sx={{overflowY: 'scroll', height: '100%'}}>
-        <Grid item xs={12} sx={{marginBottom: 2}}>
-          <Typography variant={'h2'} component={'h2'} >Recepti</Typography>
+      <Grid item xs={11} sx={{overflowY: 'scroll', height: '100%', paddingBottom: 8, paddingTop: 3}}>
+        <Grid item xs={12} sx={{
+          marginBottom: 2,
+        }}>
+          <Grid item xs={12} sx={{display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            columnGap: 3
+          }}>
+            <Grid item xs={8}>
+              <FormControl fullWidth>
+                <StyledTextField id="search-bar" label="Find something you like ..." variant="outlined" sx={{ style: {color: '#fff'}, borderRadius: '16px'}} />
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={2}>
+              <FormControl fullWidth>
+                {/*<WhiteInputLabel id="category-select-label" sx={{ color: 'white' }}>Category</WhiteInputLabel>*/}
+                <WhiteSelect
+                  // labelId="category-select-label"
+                  id="category-select"
+                  value={selectedCategory}
+                  label="Category"
+                  onChange={handleCategoryChange}
+                  sx={{color: '#fff', borderColor: '#fff'}}
+                >
+                  <MenuItem value={'--'}>
+                    Choose a category
+                  </MenuItem>
+                  {categories.map((categoryItem: string) => {
+                    return (
+                      <MenuItem value={categoryItem}>
+                        {categoryItem}
+                      </MenuItem>
+                    )
+                  })}
+                </WhiteSelect>
+              </FormControl>
+            </Grid>
+          </Grid>
         </Grid>
 
         <Grid item xs={12}>
@@ -54,12 +173,13 @@ const RecipesPage: FC<RecipesPageProps> = () => {
             justifyContent: 'space-evenly',
             paddingLeft: '5%',
             paddingRight: '5%',
-            rowGap: 4,
-            columnGap: 2
+            rowGap: 15,
+            columnGap: 2,
+            paddingTop: '150px'
           }}>
             {recipes && recipes.map((recipe) => {
               return (
-                <Grid key={recipe._id} item xs={12} sm={12} md={4} lg={2} onClick={() => { handleOnClickOnRecipe(recipe._id) }}>
+                <Grid key={recipe._id} item xs={12} sm={12} md={4} lg={2} onClick={() => { handleOnClickOnRecipe(recipe._id) }} sx={{cursor: 'pointer'}}>
                   <RecipeDisplay recipe={recipe} />
                 </Grid>
               )
