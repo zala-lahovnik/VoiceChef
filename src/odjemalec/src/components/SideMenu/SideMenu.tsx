66ffeb6c192d5dd6cb7612f56c69acebd648854d
zalalahovnik <zala.lahovnik@gmail.com>
2024-05-22 23:52:35 +0200

    feat: change side menu styles

--- src/odjemalec/src/components/SideMenu/SideMenu.tsx
+++ src/odjemalec/src/components/SideMenu/SideMenu.tsx
@@ -34,107 +34,195 @@ const SideMenu = () => {
         backgroundColor: '#252836',
         width: '100%',
         justifyContent: 'center'
-      }}>
+        }}>
         <Grid item xs={12}  sx={{
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
           borderBottomRightRadius: routePath.pathname === '/' ? '16px' : '',
           paddingTop: 2,
-          paddingBottom: 4,
+          paddingBottom: 2,
           backgroundColor: '#1F1D2B'
         }}>
         {/*  EMPTY GRID ITEM TO GET BORDERS ON TOP  DO NOT DELETE! */}
         </Grid>
-        <Grid item xs={12} sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: '#1F1D2B',
-          marginLeft: routePath.pathname === '/' ? 0 : 2,
-        }}>
-          <Grid item xs={12} sx={{
-            padding: 2,
+        <Grid item xs={12} sx={routePath.pathname === '/' ? {
             display: 'flex',
-            marginLeft: 1.5,
-            paddingRight: 3.5,
-            backgroundColor: routePath.pathname === '/' ? '#252836': '',
-            borderTopLeftRadius: routePath.pathname === '/' ? '16px' : '',
-            borderBottomLeftRadius: routePath.pathname === '/' ? '16px' : '',
             justifyContent: 'center',
-          }}>
-            <IconButton sx={{padding: 2, backgroundColor: routePath.pathname === '/' ? '#EA7C69' : '', borderRadius: '8px', filter: 'drop-shadow(0px 8px 24px rgba(234, 124, 105, 0.32))'}}>
+            alignItems: 'center',
+            backgroundColor: '#1F1D2B',
+            marginLeft: routePath.pathname === '/' ? 0 : 2,
+          } :
+          {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderTopRightRadius: routePath.pathname === '/' ? '16px' : '',
+            paddingTop: 2,
+            paddingBottom: 2,
+            backgroundColor: '#1F1D2B'
+          }
+          }>
+          <Grid item xs={12} sx={ routePath.pathname === '/' ? {
+              padding: 2,
+              display: 'flex',
+              marginLeft: 1.5,
+              paddingRight: 3.5,
+              backgroundColor: routePath.pathname === '/' ? '#252836': '',
+              borderTopLeftRadius: routePath.pathname === '/' ? '16px' : '',
+              borderBottomLeftRadius: routePath.pathname === '/' ? '16px' : '',
+              justifyContent: 'center',
+            }
+            :
+            {
+              padding: 2,
+              backgroundColor: routePath.pathname === '/shopping-list' ? '#EA7C69' : '',
+              borderRadius: '8px',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }
+          }>
+            <IconButton sx={{padding: 2, backgroundColor: routePath.pathname === '/' ? '#d17a22' : '', borderRadius: '8px', filter: 'drop-shadow(0px 8px 24px rgba(234, 124, 105, 0.32))'}}>
               <HomeRoundedIcon sx={{color: '#fff', fontSize: '36px'}} />
             </IconButton>
           </Grid>
         </Grid>
-        <Grid item xs={12}  sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderTopRightRadius: routePath.pathname === '/' ? '16px' : '',
-          paddingTop: 2,
-          paddingBottom: 4,
-          backgroundColor: '#1F1D2B'
-        }}>
-          <IconButton sx={{
-            padding: 2,
-            backgroundColor: routePath.pathname === '/shopping-list' ? '#EA7C69' : '',
-            borderRadius: '8px',
+        <Grid item xs={12} sx={routePath.pathname === 'shopping-list' ? {
             display: 'flex',
             justifyContent: 'center',
-            alignItems: 'center'
-          }}>
-            <ChecklistRoundedIcon sx={{color: '#fff', fontSize: '36px'}} />
-          </IconButton>
-        </Grid>
-        <Grid item xs={12}  sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderTopRightRadius: routePath.pathname === '/shopping-list' ? '16px' : '',
-          paddingTop: 2,
-          paddingBottom: 4,
-          backgroundColor: '#1F1D2B'
-        }}>
-          <IconButton sx={{
-            padding: 2,
-            backgroundColor: routePath.pathname === '/recipes' ? '#EA7C69' : '',
-            borderRadius: '8px',
+            alignItems: 'center',
+            backgroundColor: '#1F1D2B',
+            marginLeft: routePath.pathname === 'shopping-list' ? 0 : 2,
+          } :
+          {
             display: 'flex',
             justifyContent: 'center',
-            alignItems: 'center'
-          }}>
-            <MenuBookRoundedIcon sx={{color: '#fff', fontSize: '36px'}} />
-          </IconButton>
+            alignItems: 'center',
+            borderTopRightRadius: routePath.pathname === '/' ? '16px' : '',
+            borderBottomRightRadius: routePath.pathname.includes('/recipe/') ? '16px': '',
+            paddingTop: 2,
+            paddingBottom: 2,
+            backgroundColor: '#1F1D2B'
+          }
+        }>
+          <Grid item xs={12} sx={ routePath.pathname === 'shopping-list' ? {
+              padding: 2,
+              display: 'flex',
+              marginLeft: 1.5,
+              paddingRight: 3.5,
+              backgroundColor: '#252836',
+              borderTopLeftRadius: '16px',
+              borderBottomLeftRadius: '16px',
+              justifyContent: 'center',
+            }
+            :
+            {
+              padding: 2,
+              backgroundColor: routePath.pathname === 'shopping-list' ? '#EA7C69' : '',
+              borderRadius: '8px',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }
+          }>
+            <IconButton sx={{padding: 2, backgroundColor: routePath.pathname === 'shopping-list' ? '#d17a22' : '', borderRadius: '8px', filter: 'drop-shadow(0px 8px 24px rgba(234, 124, 105, 0.32))'}}>
+              <ChecklistRoundedIcon sx={{color: '#fff', fontSize: '36px'}} />
+            </IconButton>
+          </Grid>
         </Grid>
+        <Grid item xs={12} sx={routePath.pathname.includes('/recipe/') ? {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#1F1D2B',
+            marginLeft: routePath.pathname.includes('/recipe/') ? 0 : 2,
+          } :
+          {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderTopRightRadius: routePath.pathname === '/shopping-list' ? '16px' : '',
+            paddingTop: 2,
+            paddingBottom: 2,
+            backgroundColor: '#1F1D2B'
+          }
+        }>
+          <Grid item xs={12} sx={ routePath.pathname.includes('/recipe/') ? {
+              padding: 2,
+              display: 'flex',
+              marginLeft: 1.5,
+              paddingRight: 3.5,
+              backgroundColor: '#252836',
+              borderTopLeftRadius: '16px',
+              borderBottomLeftRadius: '16px',
+              justifyContent: 'center',
+            }
+            :
+            {
+              padding: 2,
+              backgroundColor: routePath.pathname.includes('/recipe/') ? '#EA7C69' : '',
+              borderRadius: '8px',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }
+          }>
+            <IconButton sx={{padding: 2, backgroundColor: routePath.pathname.includes('/recipe/') ? '#d17a22' : '', borderRadius: '8px', filter: 'drop-shadow(0px 8px 24px rgba(234, 124, 105, 0.32))'}}>
+              <MenuBookRoundedIcon sx={{color: '#fff', fontSize: '36px'}} />
+            </IconButton>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} sx={routePath.pathname === '/profile' ? {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#1F1D2B',
+            marginLeft: routePath.pathname === '/profile' ? 0 : 2,
+          } :
+          {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderTopRightRadius: routePath.pathname.includes('/recipe/') ? '16px' : '',
+            paddingTop: 2,
+            paddingBottom: 2,
+            backgroundColor: '#1F1D2B'
+          }
+        }>
+          <Grid item xs={12} sx={ routePath.pathname === '/profile' ? {
+              padding: 2,
+              display: 'flex',
+              marginLeft: 1.5,
+              paddingRight: 3.5,
+              backgroundColor: '#252836',
+              borderTopLeftRadius: '16px',
+              borderBottomLeftRadius: '16px',
+              justifyContent: 'center',
+            }
+            :
+            {
+              padding: 2,
+              backgroundColor: routePath.pathname === '/profile' ? '#EA7C69' : '',
+              borderRadius: '8px',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }
+          }>
+            <IconButton sx={{padding: 2, backgroundColor: routePath.pathname === '/profile' ? '#d17a22' : '', borderRadius: '8px', filter: 'drop-shadow(0px 8px 24px rgba(234, 124, 105, 0.32))'}}>
+              <ManageAccountsRoundedIcon sx={{color: '#fff', fontSize: '36px'}} />
+            </IconButton>
+          </Grid>
+        </Grid>
+
         <Grid item xs={12}  sx={{
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
           borderTopRightRadius: routePath.pathname === '/recipes' ? '16px' : '',
           paddingTop: 2,
-          paddingBottom: 4,
-          backgroundColor: '#1F1D2B'
-        }}>
-          <IconButton sx={{
-            padding: 2,
-            backgroundColor: routePath.pathname === '/settings' ? '#EA7C69' : '',
-            borderRadius: '8px',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
-          }}>
-            <ManageAccountsRoundedIcon sx={{color: '#fff', fontSize: '36px'}} />
-          </IconButton>
-        </Grid>
-        <Grid item xs={12}  sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderTopRightRadius: routePath.pathname === '/recipes' ? '16px' : '',
-          paddingTop: 2,
-          paddingBottom: 4,
+          paddingBottom: 2,
           backgroundColor: '#1F1D2B'
         }}>
           {isAuthenticated ? <IconButton sx={{
@@ -147,18 +235,18 @@ const SideMenu = () => {
           }}>
             <LogoutRoundedIcon sx={{color: '#fff', fontSize: '36px'}}/>
           </IconButton>
-            :
-            <IconButton sx={{
-              padding: 2,
-              backgroundColor: routePath.pathname === '/settings' ? '#EA7C69' : '',
-              borderRadius: '8px',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center'
-            }}>
-              <LoginRoundedIcon sx={{color: '#fff', fontSize: '36px'}}/>
-            </IconButton>
-          }
+          :
+          <IconButton sx={{
+            padding: 2,
+            backgroundColor: routePath.pathname === '/settings' ? '#EA7C69' : '',
+            borderRadius: '8px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
+            <LoginRoundedIcon sx={{color: '#fff', fontSize: '36px'}}/>
+          </IconButton>
+        }
         </Grid>
       </Grid>
     </Grid>
