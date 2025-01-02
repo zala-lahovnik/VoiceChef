c179b422c07636a22f09e2decacb825b29668616
Lonzila <57890889+Lonzila@users.noreply.github.com>
2025-01-02 13:25:13 +0100

    Update AddRecipe.tsx
    remove event

--- src/odjemalec/src/pages/AddRecipe.tsx
+++ src/odjemalec/src/pages/AddRecipe.tsx
@@ -84,13 +84,6 @@ export const DynamicField = styled('div')({
 const AddRecipe: React.FC = () => {
   const navigate = useNavigate();
   const { user } = useAuth0(); // Get the logged-in user
-
-    useEffect(() => {
-      //@ts-ignore
-      if (window.hj) {
-        window.hj('event', 'AB_checkout_Dec21_control');
-      }
-  }, []);
   
   const [recipeData, setRecipeData] = useState<RecipeData>({
     userId: user?.sub || '', // Initialize with user ID if available
